e8b5a0214a8e2ef5149eab0f7a1920a06c5ca4e3
Niklas Mikael <nikke.larsson@gmail.com>
2022-01-23 22:30:30 +0200

    Add reader

---
+++ hsltimes/reader.py
@@ -0,0 +1,33 @@
+"""Routines for reading in files."""
+
+
+import abc
+
+
+class Reader(abc.ABC):
+    """Defines an interface all the Reader subclasses should adhere to."""
+
+    @abc.abstractmethod
+    def read_data(self, filename: str) -> object:
+        pass
+
+
+class TextFileReader(Reader):
+    """Reader for reading data from a text file."""
+
+    def __init__(self) -> None:
+        """Initializes TextFileReader for reading."""
+        self.encoding: str = "utf-8"
+
+    def read_data(self, filename: str) -> object:
+        """
+        Reads in a text file.
+
+        Parameters:
+            filename.... File to read from.
+
+        Returns:
+            The contents of the text file as a generator object.
+        """
+        with open(filename, "r", encoding=self.encoding) as f:
+            return (i for i in f.read().split("\n"))
